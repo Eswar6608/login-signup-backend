59370b845ad8064642255e9686e35dfa506c33f5
Eswaran A <156338551+Eswar6608@users.noreply.github.com>
2024-08-09 20:12:47 +0530

    Add files via upload

---
+++ model/Employee.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose");
+
+const EmployeeSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: "name is required",
+    unique: true,
+  },
+
+  email: {
+    type: String,
+    required: "email is required",
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: "password is required",
+    unique: true,
+  },
+});
+const EmployeeModel = mongoose.model("employees", EmployeeSchema);
+module.exports = EmployeeModel;
